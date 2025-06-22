05bbb918ffe33bcf9d17a1f9c0029e36d79ec2e9
Vlad <koooornienko@gmail.com>
2025-06-22 14:59:38 +0500

    fix

--- src/components/mainPage/mainPageComponents/Catalog/Catalog.tsx
+++ src/components/mainPage/mainPageComponents/Catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import { Swiper, SwiperSlide } from "swiper/react";
 import { CatalogButton } from "../../../buttons/Buttons";
 import { typeChoiceTypeHouse } from "../../../catalogPage/CatalogPage";
 import { H2 } from "../../../titles/Titles";
@@ -8,23 +9,31 @@ export function Catalog({ setActiveTab }: { setActiveTab: React.Dispatch<React.S
     <section className={styles.section}>
       <H2 text={"Каталог готовых проектов"} size="big" />
       <div className={styles.wrapper}>
-        <div className={styles.tile}>
-          <img src={"./img/catalog_1.jpg"} alt="" />
-          <p className={styles.name}>1 этаж</p>
-          <CatalogButton text={"Зайти"} link={"/catalog"} zoom={0.5} setActiveTab={setActiveTab} state={{ type: "cottage" }} />
-        </div>
-        <div className={styles.tile}>
-          <img src={"./img/catalog_3.jpg"} alt="" />
+        <Swiper slidesPerView={2} spaceBetween={50} loop={true}>
+          <SwiperSlide>
+            <div className={styles.tile}>
+              <img src={"./img/catalog_1.jpg"} alt="" />
+              <p className={styles.name}>1 этаж</p>
+              <CatalogButton text={"Зайти"} link={"/catalog"} zoom={0.5} setActiveTab={setActiveTab} state={{ type: "cottage" }} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide style={{ flexBasis: "50%" }}>
+            <div className={styles.tile}>
+              <img src={"./img/catalog_3.jpg"} alt="" />
 
-          <p className={styles.name}>2 этажа</p>
-          <CatalogButton text={"Зайти"} link={"/catalog"} zoom={0.5} setActiveTab={setActiveTab} state={{ type: "two-storey house" }} />
-        </div>
-        <div className={styles.tile}>
-          <img src={"./img/catalog_2.jpg"} alt="" />
+              <p className={styles.name}>2 этажа</p>
+              <CatalogButton text={"Зайти"} link={"/catalog"} zoom={0.5} setActiveTab={setActiveTab} state={{ type: "two-storey house" }} />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className={styles.tile}>
+              <img src={"./img/catalog_2.jpg"} alt="" />
 
-          <p className={styles.name}>Бани</p>
-          <CatalogButton text={"Зайти"} link={"/catalog"} zoom={0.5} setActiveTab={setActiveTab} state={{ type: "bathhouse" }} />
-        </div>
+              <p className={styles.name}>Бани</p>
+              <CatalogButton text={"Зайти"} link={"/catalog"} zoom={0.5} setActiveTab={setActiveTab} state={{ type: "bathhouse" }} />
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
       <div id="catalog" style={{ position: "absolute", top: "-100px" }}></div>
     </section>
